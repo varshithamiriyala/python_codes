19f8cb28d3f54c095fc646fc9227be290eb63803
Miriyala Varshitha Jeevan <varshithamiriyala28@gmail.com>
2026-09-08 12:45:13 +0530

    Delete 5I9 directory

--- 5I9/electronics.py
+++
@@ -1,9 +0,0 @@
-class Electronics:
-    def turnOn():
-        print('device is on')
-class Mobile(Electronics):
-    def takePics():
-        print('clicked pics')
-poco=Mobile
-poco.turnOn()
-poco.takePics()
--- 5I9/fruit.py
+++
@@ -1,3 +0,0 @@
-fruits=['apple','orange','kiwi','mango','cherry']
-fruit=[ch for ch in fruits if 'a' in ch]
-print(fruit)
